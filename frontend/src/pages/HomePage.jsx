c13d4c29a381cc5ae3f6e19a2756d9c3e3d05d19
arifazfar99 <arifazfar99@gmail.com>
2025-02-25 13:42:25 +0800

    initial commit

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import LoginPage from "./LoginPage";
+
+import classes from './HomePage.module.css'
+
+function HomePage() {
+  return (
+    <div className={classes.container}>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default HomePage;
