d9ea529b8e21db2114b737f39d124b4ce08ea779
almazuulu <almazuulu@gmail.com>
2025-03-05 14:09:09 +0800

    Removed all the frontend folders and files

--- frontend/src/PublicRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { isAuthenticated } from "./utils/Authentication";
-
-const PublicRoute = () => {
-  return isAuthenticated() ? <Navigate to="/dashboard" replace /> : <Outlet />;
-};
-
-export default PublicRoute;
--- frontend/src/pages/PlayerDetails.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function PlayerDetailsPage() {
-  return (
-    <div>PlayerDetailsPage</div>
-  )
-}
-
-export default PlayerDetailsPage
